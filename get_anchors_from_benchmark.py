395efa3870d8ddf08d87c397d260ec89f1f21f34
Wen Wen <30362457+nomadseed@users.noreply.github.com>
2018-11-18 08:56:53 -0500

    Add files via upload
    add function to display the width and height distribution

--- get_anchors_from_benchmark.py
+++ get_anchors_from_benchmark.py
@@ -11,6 +11,7 @@ generate certain numbers of anchors according to the histogram
 import json
 import os
 import argparse
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -60,7 +61,7 @@ def generate_aspect_ratios(mean, std):
         aspectratios.append(round(mean+i*std,2))
     return aspectratios
 
-def generate_histogram_dict(anchorratios, 
+def generate_histogram_dict(bboxes, 
                             numbins=200, 
                             minratio=0.3, 
                             maxratio=3.0,
@@ -79,7 +80,7 @@ def generate_histogram_dict(anchorratios,
     '''
     # build histogram
     ratiolist=[]
-    for ratio in anchorratios:
+    for ratio in bboxes:
         ratiolist.append(ratio['ratio'])                
     hist=np.histogram(ratiolist,numbins,(minratio,maxratio))
     
@@ -96,27 +97,100 @@ def generate_histogram_dict(anchorratios,
         plt.show()   
     
     return hist, mean, std
-   
-def get_anchors_ratios(annotationdict):
+
+def generate_bbox_size_histogram(bboxes, 
+                                 numbins=200,
+                                 rejsize_h=22,
+                                 rejsize_w=22,
+                                 debug=True):
+    '''
+    
+    
+    ^
+    |     #
+    |    # # 
+    |   # # # #  
+    |  # # # # # #  
+    | # # # # # # # # # # # # # 
+    -------------------------------->
+    0                          maxsize  (w or h)
+    '''
+    hlist=[]
+    wlist=[]
+    diaglist=[]
+    totalbox=len(bboxes)
+    wreject=0
+    hreject=0
+    allreject=0
+    for bbox in bboxes:
+        hlist.append(bbox['h'])
+        wlist.append(bbox['w'])
+        diaglist.append(bbox['diag'])
+        if bbox['h']<rejsize_h:
+            hreject+=1
+        if bbox['w']<rejsize_w:
+            wreject+=1
+        if bbox['w']<rejsize_w and bbox['h']<rejsize_h:
+            allreject+=1
+    h_hist=np.histogram(hlist,numbins,(0,480))
+    w_hist=np.histogram(wlist,numbins,(0,640))
+    diag_hist=np.histogram(diaglist,numbins,(0,800))
+    
+    # plot the chart for debug
+    if debug:
+        plt.hist(hlist,numbins,(0,480)) 
+        plt.title('bbox height Histogram')
+        plt.xlabel('pixel size')
+        plt.ylabel('BBox count')
+        plt.show()
+        
+        plt.hist(wlist,numbins,(0,640)) 
+        plt.title('bbox width Histogram')
+        plt.xlabel('pixel size')
+        plt.ylabel('BBox count')
+        plt.show()
+        
+        plt.hist(wlist,numbins,(0,800)) 
+        plt.title('bbox diagonal Histogram')
+        plt.xlabel('pixel size')
+        plt.ylabel('BBox count')
+        plt.show()
+    
+    # print the percent of rejected boxes
+    print('{}% bbox rejected by width'.format(wreject/totalbox*100))
+    print('{}% bbox rejected by height'.format(hreject/totalbox*100))
+    print('{}% bbox rejected by both width and height'.format(allreject/totalbox*100))
+    
+    
+    return h_hist,w_hist, diag_hist
+
+
+def get_bboxes(annotationdict):
     '''
     
     anchorratios=[{'class':'car',
                    'image':'xxx.jpg',
                    'ratio':0.5}, {}, {} ]
     '''
-    anchorratios=[]
+    bboxes=[]
     benchmarkanchor={}
     for foldername in annotationdict:
         for imagename in annotationdict[foldername]:
-            for anno in annotationdict[foldername][imagename]['annotations']:
-                benchmarkanchor['class']=anno['label'].lower()
-                benchmarkanchor['image']=imagename
-                if anno['width'] and anno['height']:
-                    benchmarkanchor['ratio']=anno['width']/anno['height']
-                    anchorratios.append(benchmarkanchor)
-                benchmarkanchor={}
+            if len(annotationdict[foldername][imagename])==0:
+                continue
+            else:
+                for anno in annotationdict[foldername][imagename]['annotations']:
+                    benchmarkanchor['class']=anno['label'].lower()
+                    benchmarkanchor['image']=imagename
+                    if anno['width'] and anno['height']:
+                        benchmarkanchor['ratio']=anno['width']/anno['height']
+                        benchmarkanchor['w']=anno['width']
+                        benchmarkanchor['h']=anno['height']
+                        benchmarkanchor['diag']=math.sqrt(anno['width']*anno['width']+anno['height']*anno['height'])
+                        bboxes.append(benchmarkanchor)
+                    benchmarkanchor={}
                 
-    return anchorratios
+    return bboxes
 
 def load_json_annotations(filepath, jsonlabel):
     
@@ -125,22 +199,24 @@ def load_json_annotations(filepath, jsonlabel):
     folderdict=os.listdir(filepath)
     for foldername in folderdict:
         jsonpath=os.path.join(filepath,foldername)
+        if '.' in jsonpath:
+            continue
         # load the json files
-        if not os.path.exists(os.path.join(jsonpath,'annotationfull_'+foldername+'.json')):
-            continue   
-        else:
-            annotationdict[foldername]={}
-            annotationdict[foldername]=json.load(open(os.path.join(jsonpath,'annotationfull_'+foldername+'.json')))
+        jsondict=os.listdir(jsonpath)
+        for jsonname in jsondict:
+            if jsonlabel in jsonname and '.json'in jsonname:
+                annotationdict[foldername]={}
+                annotationdict[foldername]=json.load(open(os.path.join(jsonpath,jsonname)))
     
     return annotationdict
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--file_path', type=str, 
-                        default='D:/Private Manager/Personal File/uOttawa/Lab works/2018 summer/Leading Vehicle/Viewnyx dataset/EventFrames', 
+                        default='D:/Private Manager/Personal File/uOttawa/Lab works/2018 fall/BerkleyDeepDrive/debug/bdd100k', 
                         help="File path of input data")
     parser.add_argument('--json_label', type=str, 
-                        default='with_leading', 
+                        default='crop', 
                         help="label to specify json file")
     args = parser.parse_args()
     
@@ -151,14 +227,18 @@ if __name__=='__main__':
     annotationdict = load_json_annotations(filepath, jsonlabel)
     
     # get anchor ratios
-    anchorratios = get_anchors_ratios(annotationdict)      
+    bboxes = get_bboxes(annotationdict)      
     
     # generate histogram (bins for different aspect ratios)
-    hist, mean, std = generate_histogram_dict(anchorratios)
+    hist, mean, std = generate_histogram_dict(bboxes)
     
     # generate aspect ratios for the training
     aspectratios = generate_aspect_ratios(mean, std)      
 
     print('suggested anchor aspect ratios:\n',aspectratios)
-
+    
+    # generate histogram of bbox size
+    h_hist, w_hist, _ = generate_bbox_size_histogram(bboxes)
+    
+    
 """ End of File """
